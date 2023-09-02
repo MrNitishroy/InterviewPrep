2195530ed5029cc5ca3755bb50799843aa445ceb
mrnitishroy <mr.nitishkrs@gmail.com>
2023-09-02 23:37:43 +0530

    #13 interview tips page completed

--- lib/Pages/InterviewPage/InterivewTips.dart
+++ lib/Pages/InterviewPage/InterivewTips.dart
@@ -1,7 +1,11 @@
+import 'package:chatgpt/Components/BackButton.dart';
+import 'package:chatgpt/Components/MyButton.dart';
+import 'package:chatgpt/Controller/CameraController.dart';
 import 'package:chatgpt/Controller/InterviewController.dart';
 import 'package:chatgpt/Pages/InterviewPage/Interview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 import '../../Model/QuestionModel.dart';
@@ -13,16 +17,128 @@ class InterviewTips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     InterviewController interviewController = Get.put(InterviewController());
+    ICameraController iCameraController = Get.put(ICameraController());
     interviewController.questionLevel = e;
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            Get.to(InterviewPage(e: e));
-          },
-          child: Text("I UNDESTAND GO TO INTERVIEW PAGE"),
-        ),
+        body: Padding(
+      padding: const EdgeInsets.all(40),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              MyBackButton(),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "Assets/Image/sitting.png",
+                width: context.width * 0.7,
+              ),
+            ],
+          ),
+          SizedBox(height: 30),
+          Text(
+            "Things to keep in mind",
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SvgPicture.asset(
+                "Assets/Icons/check.svg",
+                width: 20,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              SizedBox(width: 10),
+              Text(
+                "Prepare you self for the interview",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SvgPicture.asset(
+                "Assets/Icons/check.svg",
+                width: 20,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              SizedBox(width: 10),
+              Text(
+                "All Question are related to your profile",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SvgPicture.asset(
+                "Assets/Icons/check.svg",
+                width: 20,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              SizedBox(width: 10),
+              Text(
+                "Keep straight and look at the camera",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SvgPicture.asset(
+                "Assets/Icons/check.svg",
+                width: 20,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              SizedBox(width: 10),
+              Text(
+                "Keep your background clean and tidy",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SvgPicture.asset(
+                "Assets/Icons/check.svg",
+                width: 20,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              SizedBox(width: 10),
+              Text(
+                "Keep your phone on silent mode",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ],
+          ),
+          Spacer(),
+          InkWell(
+            onTap: () {
+              Get.to(InterviewPage(e: e));
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Text(
+                "I AM READY",
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            ),
+          )
+        ],
       ),
-    );
+    ));
   }
 }
