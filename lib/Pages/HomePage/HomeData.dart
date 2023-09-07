1a1232b149578d89fcf149de5c7f9994ad11b90c
mrnitishroy <mr.nitishkrs@gmail.com>
2023-09-07 23:41:47 +0530

    home safe area

--- lib/Pages/HomePage/HomeData.dart
+++ lib/Pages/HomePage/HomeData.dart
@@ -24,94 +24,96 @@ class HomeData extends StatelessWidget {
 
     return Scaffold(
       drawer: PrimaryDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: 30),
-              HeaderUserInfo(
-                openDrawer: openDrawer,
-              ),
-              SizedBox(height: 20),
-              // UserInfo(),
-              // SizedBox(height: 20),
-              Row(
-                children: [
-                  Text(
-                    "Featurs post",
-                    style: Theme.of(context).textTheme.labelMedium,
-                  )
-                ],
-              ),
-              CarouselSlider(
-                items: homeController.slideItem,
-                options: CarouselOptions(
-                  height: 140,
-                  aspectRatio: 16 / 9,
-                  viewportFraction: 0.9,
-                  initialPage: 0,
-                  enableInfiniteScroll: true,
-                  reverse: false,
-                  autoPlay: true,
-                  autoPlayInterval: Duration(seconds: 6),
-                  autoPlayAnimationDuration: Duration(milliseconds: 800),
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                  enlargeCenterPage: true,
-                  enlargeFactor: 0.3,
-                  onPageChanged: (page, pagename) {},
-                  scrollDirection: Axis.vertical,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // SizedBox(height: 30),
+                HeaderUserInfo(
+                  openDrawer: openDrawer,
                 ),
-              ),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  Text(
-                    "Prepare Speeking",
-                    style: Theme.of(context).textTheme.labelMedium,
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Column(
-                children: QuestionData.map(
-                  (e) => InterviewCategory(
-                    ontap: () {
-                      Get.to(QuestionLevelPage(
-                        data: e,
-                      ));
-                    },
-                    title: e.title.toString(),
-                    iconPath: "Assets/Icons/lock.svg",
-                  ),
-                ).toList(),
-              ),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  Text(
-                    "Tens Practice",
-                    style: Theme.of(context).textTheme.labelMedium,
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Column(
-                children: tensQuestions
-                    .map(
-                      (e) => InterviewCategory(
-                        ontap: () {
-                          Get.to(QuestionLevelPage(
-                            data: e,
-                          ));
-                        },
-                        title: e.title.toString(),
-                        iconPath: "Assets/Icons/lock.svg",
-                      ),
+                SizedBox(height: 20),
+                // UserInfo(),
+                // SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      "Featurs post",
+                      style: Theme.of(context).textTheme.labelMedium,
                     )
-                    .toList(),
-              ),
-            ],
+                  ],
+                ),
+                CarouselSlider(
+                  items: homeController.slideItem,
+                  options: CarouselOptions(
+                    height: 140,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.9,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 6),
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.3,
+                    onPageChanged: (page, pagename) {},
+                    scrollDirection: Axis.vertical,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      "Prepare Speeking",
+                      style: Theme.of(context).textTheme.labelMedium,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Column(
+                  children: QuestionData.map(
+                    (e) => InterviewCategory(
+                      ontap: () {
+                        Get.to(QuestionLevelPage(
+                          data: e,
+                        ));
+                      },
+                      title: e.title.toString(),
+                      iconPath: "Assets/Icons/lock.svg",
+                    ),
+                  ).toList(),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      "Tens Practice",
+                      style: Theme.of(context).textTheme.labelMedium,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Column(
+                  children: tensQuestions
+                      .map(
+                        (e) => InterviewCategory(
+                          ontap: () {
+                            Get.to(QuestionLevelPage(
+                              data: e,
+                            ));
+                          },
+                          title: e.title.toString(),
+                          iconPath: "Assets/Icons/lock.svg",
+                        ),
+                      )
+                      .toList(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
